7ee9aba1d2758eda610c04fc8debb249db82a50a
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-05-10 18:44:13 -0400

    bug fixes

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -3,6 +3,20 @@
 //    * A GET Route to `/survey` which should display the survey page.
 //    * A default, cat      
 
-module.exports(app) {
+var path = require('path');
+
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+
+    });
+
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'))
+    })
     
 }
